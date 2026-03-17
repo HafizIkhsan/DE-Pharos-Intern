916e7dfba06d3d80adef1c35e4b169cbc0574d3b
Hafiz Ikhsan <hafizikhsan73@gmail.com>
2026-03-18 00:05:41 +0700

    Error Handling transform and load pipline progress 50%

--- src/etl/load.py
+++ src/etl/load.py
@@ -6,73 +6,83 @@ from psycopg2.extras import execute_batch
 load_dotenv()
 
 def load_data(df):
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
+        )
 
-    cursor = conn.cursor()
+        cursor = conn.cursor()
+    except Exception as e:
+        raise RuntimeError(f"Koneksi ke database gagal karena: {e}")
 
     ## Insert data ke database
     # Data outlet
-    outlets = df[["outlet_code", "outlet_name"]].drop_duplicates()
+    try:
+        outlets = df[["outlet_code", "outlet_name"]].drop_duplicates()
 
-    data_outlet = [
-        (row["outlet_code"], row["outlet_name"])
-        for _, row in outlets.iterrows()
-    ]
+        data_outlet = [
+            (row["outlet_code"], row["outlet_name"])
+            for _, row in outlets.iterrows()
+        ]
 
-    execute_batch(cursor, """
-        INSERT INTO outlets (outlet_code, outlet_name)
-        VALUES (%s, %s)
-        ON CONFLICT (outlet_code) DO NOTHING
-        """, data_outlet)
+        execute_batch(cursor, """
+            INSERT INTO outlets (outlet_code, outlet_name)
+            VALUES (%s, %s)
+            ON CONFLICT (outlet_code) DO NOTHING
+            """, data_outlet)
 
-    # Data product
-    products = df[["product_code", "product_name"]].drop_duplicates()
+        # Data product
+        products = df[["product_code", "product_name"]].drop_duplicates()
 
-    data_product = [
-        (row["product_code"], row["product_name"])
-        for _, row in products.iterrows()
-    ]
+        data_product = [
+            (row["product_code"], row["product_name"])
+            for _, row in products.iterrows()
+        ]
 
-    execute_batch(cursor, """
-        INSERT INTO products (product_code, product_name)
-        VALUES (%s, %s)
-        ON CONFLICT (product_code) DO NOTHING
-        """, data_product)
+        execute_batch(cursor, """
+            INSERT INTO products (product_code, product_name)
+            VALUES (%s, %s)
+            ON CONFLICT (product_code) DO NOTHING
+            """, data_product)
 
-    # Data outlet_product
-    outlet_product = df[["outlet_code", "product_code", "product_price"]].drop_duplicates()
+        # Data outlet_product
+        outlet_product = df[["outlet_code", "product_code", "product_price"]].drop_duplicates()
 
-    data_outlet_product = [
-        (row["outlet_code"], row["product_code"], row["product_price"])
-        for _, row in outlet_product.iterrows()
-    ]
+        data_outlet_product = [
+            (row["outlet_code"], row["product_code"], row["product_price"])
+            for _, row in outlet_product.iterrows()
+        ]
 
-    execute_batch(cursor, """
-        INSERT INTO outlets_products (outlet_code, product_code, product_price)
-        VALUES (%s, %s, %s)
-        ON CONFLICT (outlet_code, product_code) DO NOTHING
-        """, data_outlet_product)
-    
-    # Data sales
-    sales = df[["sales_period", "outlet_code", "product_code", "qty", "actual_sales"]].drop_duplicates()
+        execute_batch(cursor, """
+            INSERT INTO outlets_products (outlet_code, product_code, product_price)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (outlet_code, product_code) DO NOTHING
+            """, data_outlet_product)
+        
+        # Data sales
+        sales = df[["sales_period", "outlet_code", "product_code", "qty", "actual_sales"]].drop_duplicates()
 
-    data_sales = [
-        (row["sales_period"], row["outlet_code"], row["product_code"], row["qty"], row["actual_sales"])
-        for _, row in sales.iterrows()
-    ]
+        data_sales = [
+            (row["sales_period"], row["outlet_code"], row["product_code"], row["qty"], row["actual_sales"])
+            for _, row in sales.iterrows()
+        ]
 
-    execute_batch(cursor, """
-        INSERT INTO sales (sales_period, outlet_code, product_code, qty, actual_sales)
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT (sales_period, outlet_code, product_code) DO NOTHING
-        """, data_sales)
+        execute_batch(cursor, """
+            INSERT INTO sales (sales_period, outlet_code, product_code, qty, actual_sales)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (sales_period, outlet_code, product_code) DO NOTHING
+            """, data_sales)
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise RuntimeError(f"Data gagal di load ke database karena: {e}")   
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
--- src/etl/transform.py
+++ src/etl/transform.py
@@ -1,36 +1,72 @@
 import pandas as pd
 
+def mapping(df, key, value):
+    missing_value = df[key].isna().sum()
+    print(f"{missing_value} missing value pada kolom '{key}'")
+
+    if missing_value == 0:
+        print(f"Tidak ada missing value pada kolom '{key}'")
+        return df
+    
+    mapping = (
+        df.dropna(subset=[key, value])
+        .drop_duplicates(subset=[value]).
+        set_index(value)[key]
+        .to_dict()
+    )
+
+    df.loc[df[key].isna(), key] = df.loc[df[key].isna(), value].map(
+        mapping
+    )
+
+    after_mapping_missing_value = df[key].isna().sum()
+    print(f"{after_mapping_missing_value} missing value pada kolom '{key}' setelah mapping")
+
+    if after_mapping_missing_value > 0:
+        print(f"[WARNING] {after_mapping_missing_value} data gagal di-map pada {key}")
+
+    return df
+
 def transform_data(df):
+    ## Error Handling dataset yang tidak ditemukan, dataset kosong, dan kolom yang tidak sesuai
+    if df is None:
+        raise ValueError("Dataset tidak ditemukan")
+    if df.empty:
+        raise ValueError("Dataset kosong")
+    
+    required_columns = {"outlet_code", "outlet_name", "product_code", "product_name", "product_price", "sales_period (DD/MM/YYYY)", "qty", "actual_sales"}
+    actual_columns = set(df.columns)
+    missing_columns = required_columns - actual_columns
+
+    if missing_columns:
+        raise ValueError("Kolom di dataset tidak sesuai")
+
     # logging jumlah data sebelum transformasi
     print("Jumlah data sebelum transformasi:", df.shape[0])
 
-    # Handling Missing Values
+    ## Handling Missing Values
     # Mencari nilai key-value menggunakan mapping antara kolom 'outlet_code' dan 'outlet_name'
-    mapping_outlet = df.dropna(subset=["outlet_code", "outlet_name"]).drop_duplicates(subset=["outlet_name"]).set_index("outlet_name")["outlet_code"].to_dict()
-
-    df.loc[df["outlet_code"].isna(), "outlet_code"] = df.loc[df["outlet_code"].isna(), "outlet_name"].map(
-        mapping_outlet
-    )
+    df = mapping(df, "outlet_code", "outlet_name")
 
     # Mencari nilai key-value menggunakan mapping antara kolom  'product_code' dan 'product_name'
-    mapping_product = df.dropna(subset=["product_code", "product_name"]).drop_duplicates(subset=["product_name"]).set_index("product_name")["product_code"].to_dict()
-
-    df.loc[df["product_code"].isna(), "product_code"] = df.loc[df["product_code"].isna(), "product_name"].map(
-        mapping_product
-    )
+    df = mapping(df, "product_code", "product_name")
 
     """ Karena sudah tidak ada pasangan key-value lagi di database jadi kita lanjut mengurus missing value untuk data yang tidak memiliki pasangan key-value """
     # Menghapus baris yang memiliki missing value pada kolom 'outlet_code', 'product_code', dan 'product_name'
+    print(df.shape[0], "data sebelum menghapus missing value pada kolom 'outlet_code', 'product_code', dan 'product_name'")
     df = df.dropna(subset=["product_code","product_name", "outlet_code" , "outlet_name"])
-
+    print(df.shape[0], "data setelah menghapus missing value pada kolom 'outlet_code', 'product_code', dan 'product_name'")
     # Asumsi saya jika qty nya kosong dan actual_sales nya 0 maka data transaksi tersebut tidak valid dan tidak perlu dimasukkan ke dalam database, maka saya akan menghapus data tersebut
+    print(df.shape[0], "data sebelum menghapus data yang memiliki missing value pada kolom 'qty' dan 'actual_sales' bernilai 0")
     df = df.drop(df[(df["qty"].isna()) & (df["actual_sales"] == 0)].index)
+    print(df.shape[0], "data setelah menghapus data yang memiliki missing value pada kolom 'qty' dan 'actual_sales' bernilai 0")
 
-    # Handling Data Type
+    ## Handling Data Type
     # Ubah Data Type pada kolom 'sales_period (DD/MM/YYYY)' menjadi datetime
     df["sales_period (DD/MM/YYYY)"] = pd.to_datetime(
         df["sales_period (DD/MM/YYYY)"],
-        format="%d/%m/%Y"
+        format="%d/%m/%Y",
+        errors="coerce"
     )
 
     # Rename kolom 'sales_period (DD/MM/YYYY)' menjadi 'sales_period'
